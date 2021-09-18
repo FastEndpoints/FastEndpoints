210dcdab73cef10062975f0f38432ed606a0c356
Damith G <djnitehawk83@gmail.com>
2021-09-18 19:40:55 +0530

    update interfaces

--- ASPie/Interfaces.cs
+++ ASPie/Interfaces.cs
@@ -1,6 +1,17 @@
 ﻿namespace ASPie
 {
     public interface IRequest { }
+
+    public interface IRequest<TEntity> : IRequest
+    {
+        TEntity ToEntity();
+    }
     public interface IResponse { }
+
+    public interface IResponse<TEntity> : IResponse
+    {
+        void FromEntity(TEntity entity);
+    }
+
     public interface IHandler { }
 }

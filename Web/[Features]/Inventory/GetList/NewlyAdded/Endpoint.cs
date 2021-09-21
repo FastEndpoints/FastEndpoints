f38e462e555870c2df384fe6c456b668e203f247
Damith G <djnitehawk83@gmail.com>
2021-09-21 20:25:10 +0530

    wip

--- Web/[Features]/Inventory/GetList/NewlyAdded/Endpoint.cs
+++ Web/[Features]/Inventory/GetList/NewlyAdded/Endpoint.cs
@@ -16,23 +16,15 @@ namespace Inventory.GetList.NewlyAdded
             //    Allow.Inventory_Create_Item,
             //    Allow.Inventory_Retrieve_Item,
             //    Allow.Inventory_Update_Item);
-            //AllowAnnonymous();
+            AllowAnnonymous();
             //DontThrowIfValidationFails();
         }
 
         protected override Task HandleAsync(Request req, Context<Request> ctx)
         {
-            var key = Config["TokenKey"];
-            var token = JWTBearer.CreateTokenWithClaims(
-                key,
-                DateTime.Now.AddDays(1),
-                new[] { Allow.Inventory_Retrieve_Item, Allow.Inventory_Create_Item, Allow.Inventory_Update_Item },
-                new[] { "Admin", "Manager" },
-                ("TestClaimType", "TestClaimValue"));
-
             var res = new Response
             {
-                Message = token,
+                Message = req.Id.ToString(),
                 Name = req.Name,
                 Price = req.Price
             };
--- Web/[Features]/Inventory/GetList/NewlyAdded/Request.cs
+++ Web/[Features]/Inventory/GetList/NewlyAdded/Request.cs
@@ -4,6 +4,7 @@ namespace Inventory.GetList.NewlyAdded
 {
     public class Request : IRequest
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public int Price { get; set; }
     }

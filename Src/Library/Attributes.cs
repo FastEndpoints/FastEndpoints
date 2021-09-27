cd52bfae7e0cf28a38344cdc7751823a7c9716f8
Damith G <djnitehawk83@gmail.com>
2021-09-27 14:28:35 +0530

    wip

--- Src/Library/Attributes.cs
+++ Src/Library/Attributes.cs
@@ -1,11 +1,26 @@
 ﻿namespace FastEndpoints
 {
+    /// <summary>
+    /// properties decorated with this attribute will have their values auto bound from the relevant claim of the current user principal
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class FromClaimAttribute : Attribute
     {
+        /// <summary>
+        /// the claim type to auto bind
+        /// </summary>
         public string ClaimType { get; set; }
+
+        /// <summary>
+        /// set to true if a validation error should be thrown when the current user principal doesn't have the specified claim
+        /// </summary>
         public bool ForbidIfMissing { get; set; }
 
+        /// <summary>
+        /// properties decorated with this attribute will have their values auto bound from the relevant claim of the current user principal
+        /// </summary>
+        /// <param name="claimType">the claim type to auto bind</param>
+        /// <param name="forbidIfMissing">set to true if a validation error should be thrown when the current user principal doesn't have the specified claim</param>
         public FromClaimAttribute(string claimType, bool forbidIfMissing = true)
         {
             ClaimType = claimType;
@@ -13,9 +28,17 @@
         }
     }
 
+    /// <summary>
+    /// properties decorated with this attribute will have their values auto bound from the relevant claim of the current user principal
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class FromAttribute : FromClaimAttribute
     {
+        /// <summary>
+        /// properties decorated with this attribute will have their values auto bound from the relevant claim of the current user principal
+        /// </summary>
+        /// <param name="claimType">the claim type to auto bind</param>
+        /// <param name="forbidIfMissing">set to true if a validation error should be thrown when the current user principal doesn't have the specified claim</param>
         public FromAttribute(string claimType, bool forbidIfMissing = true) : base(claimType, forbidIfMissing) { }
     }
 }

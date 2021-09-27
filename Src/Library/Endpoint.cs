c63f8bb9b5c20cdfc6b1994c79cadbaa43b400e0
Damith G <djnitehawk83@gmail.com>
2021-09-27 14:59:54 +0530

    wip

--- Src/Library/Endpoint.cs
+++ Src/Library/Endpoint.cs
@@ -144,6 +144,11 @@ namespace FastEndpoints
             this.permissions = permissions;
         }
 
+        /// <summary>
+        /// the handler method for the endpoint. this method is called for each request received.
+        /// </summary>
+        /// <param name="req">the request dto</param>
+        /// <param name="ct">a cancellation token</param>
         protected abstract Task HandleAsync(TRequest req, CancellationToken ct);
 
         internal override async Task ExecAsync(HttpContext ctx, CancellationToken cancellation)
@@ -162,33 +167,58 @@ namespace FastEndpoints
             }
         }
 
+        /// <summary>
+        /// adds a "GeneralError" to the current list of validation failures
+        /// </summary>
+        /// <param name="message">the error message</param>
         protected void AddError(string message)
             => ValidationFailures.Add(new ValidationFailure("GeneralErrors", message));
 
+        /// <summary>
+        /// adds an error message for the specified property of the request dto
+        /// </summary>
+        /// <param name="property">the property to add teh error message for</param>
+        /// <param name="errorMessage">the error message</param>
         protected void AddError(Expression<Func<TRequest, object>> property, string errorMessage)
         {
             ValidationFailures.Add(
                 new ValidationFailure(property.PropertyName(), errorMessage));
         }
 
+        /// <summary>
+        /// interrupt the flow of handler execution and send a 400 bad request with error details if there are any validation failures in the current request. if there are no validation failures, execution will continue past this call.
+        /// </summary>
         protected void ThrowIfAnyErrors()
         {
             if (ValidationFailed)
                 throw new ValidationFailureException();
         }
 
+        /// <summary>
+        /// add a "GeneralError" to the validation failure list and send back a 400 bad request with error details immediately interrupting handler execution flow. if there are any vallidation failures, no execution will continue past this call.
+        /// </summary>
+        /// <param name="message">the error message</param>
         protected void ThrowError(string message)
         {
             AddError(message);
             ThrowIfAnyErrors();
         }
 
+        /// <summary>
+        /// adds an error message for the specified property of the request dto and sends back a 400 bad request with error details immediately interrupting handler execution flow. no execution will continue past this call.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="errorMessage"></param>
         protected void ThrowError(Expression<Func<TRequest, object>> property, string errorMessage)
         {
             AddError(property, errorMessage);
             ThrowIfAnyErrors();
         }
 
+        /// <summary>
+        /// send a 400 bad request with error details of the current validation failures
+        /// </summary>
+        /// <param name="cancellation"></param>
         protected Task SendErrorsAsync(CancellationToken cancellation = default)
         {
             HttpContext.Response.StatusCode = 400;

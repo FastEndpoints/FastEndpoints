c4723d9c073ccb6eb055743134972be2de729753
Damith G <djnitehawk83@gmail.com>
2021-10-16 09:41:36 +0530

    enum update

--- Src/Library/Enums.cs
+++ Src/Library/Enums.cs
@@ -5,10 +5,30 @@
 /// </summary>
 public enum Http
 {
-    GET,
-    POST,
-    PUT,
-    DELETE
+    /// <summary>
+    /// retrieve a record
+    /// </summary>
+    GET = 1,
+
+    /// <summary>
+    /// create a record
+    /// </summary>
+    POST = 2,
+
+    /// <summary>
+    /// replace a record
+    /// </summary>
+    PUT = 3,
+
+    /// <summary>
+    /// partially update a record
+    /// </summary>
+    PATCH = 4,
+
+    /// <summary>
+    /// remove a record
+    /// </summary>
+    DELETE = 5
 }
 
 /// <summary>

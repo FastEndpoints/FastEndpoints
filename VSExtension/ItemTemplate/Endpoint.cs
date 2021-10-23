f405540a5ca3d17001b4a1eb185e04535e67d71a
Damith G <djnitehawk83@gmail.com>
2021-10-23 16:22:36 +0530

    add azure release pipeline

--- VSExtension/ItemTemplate/Endpoint.cs
+++ VSExtension/ItemTemplate/Endpoint.cs
@@ -4,8 +4,7 @@ public class Endpoint : Endpoint<Request, Response>
 {
     public override void Configure()
     {
-        Verbs(Http.GET);
-        Routes("/route/path/here");
+        Post("/route/path/here");
     }
 
     public override Task HandleAsync(Request r, CancellationToken c)

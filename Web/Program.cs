734b3cd5f6e2cf8db58570c199c6ff0d19f781d6
Damith G <djnitehawk83@gmail.com>
2021-11-09 21:10:28 +0530

    remove dto code gen - experiment failed

--- Web/Program.cs
+++ Web/Program.cs
@@ -5,7 +5,6 @@ global using Web.Auth;
 using FastEndpoints.Swagger;
 using Microsoft.AspNetCore.Http.Json;
 using Swashbuckle.AspNetCore.SwaggerUI;
-using System.Text.Json.Serialization;
 using Web.Services;
 
 var builder = WebApplication.CreateBuilder();
@@ -14,11 +13,7 @@ builder.Services.AddResponseCaching();
 builder.Services.AddFastEndpoints();
 builder.Services.AddAuthenticationJWTBearer(builder.Configuration["TokenKey"]);
 builder.Services.AddAuthorization(o => o.AddPolicy("AdminOnly", b => b.RequireRole(Role.Admin)));
-builder.Services.Configure<JsonOptions>(o =>
-{
-    o.SerializerOptions.PropertyNamingPolicy = null;
-    o.SerializerOptions.AddContext<SerializerContext>();
-});
+builder.Services.Configure<JsonOptions>(o => o.SerializerOptions.PropertyNamingPolicy = null);
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddSwagger();
 
@@ -39,11 +34,3 @@ if (!app.Environment.IsProduction())
     });
 }
 app.Run();
-
-[
-    JsonSerializable(typeof(Admin.Login.Request)),
-    JsonSerializable(typeof(Admin.Login.Response)),
-    JsonSerializable(typeof(Customers.Create.Request)),
-    JsonSerializable(typeof(Customers.List.Recent.Response))
-]
-public partial class SerializerContext : JsonSerializerContext { }

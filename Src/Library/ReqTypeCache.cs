26eccff58587c297f0f2eaaca9094c3bc9a0daa4
Damith G <djnitehawk83@gmail.com>
2021-12-19 13:43:37 +0530

    prepare ReqTypeCache.cs for FromHeader binding

--- Src/Library/ReqTypeCache.cs
+++ Src/Library/ReqTypeCache.cs
@@ -13,12 +13,19 @@ internal record FromClaimPropCacheEntry(
     bool ForbidIfMissing,
     Action<object, object> PropSetter);
 
+internal record FromHeaderPropCacheEntry(
+    string HeaderName,
+    bool ForbidIfMissing,
+    Action<object, object> PropSetter);
+
 internal static class ReqTypeCache<TRequest>
 {
     //note: key is lowercased property name
     internal static Dictionary<string, PropCacheEntry> CachedProps { get; } = new();
 
     internal static List<FromClaimPropCacheEntry> CachedFromClaimProps { get; } = new();
+
+    internal static List<FromHeaderPropCacheEntry> CachedFromHeaderProps { get; } = new();
 
     static ReqTypeCache()
     {
@@ -27,26 +34,55 @@ internal static class ReqTypeCache<TRequest>
         foreach (var propInfo in reqType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy))
         {
             if (!propInfo.CanRead || !propInfo.CanWrite)
-                return;
+                continue;
 
             var propName = propInfo.Name;
             var compiledSetter = reqType.SetterForProp(propName);
 
+            if (AddFromClaimPropCacheEntry(propInfo, propName, compiledSetter))
+                continue;
+
+            if (AddFromHeaderPropCacheEntry(propInfo, propName, compiledSetter))
+                continue;
+
             AddPropCacheEntry(propInfo, propName, compiledSetter);
-
-            if (propInfo.IsDefined(typeof(FromClaimAttribute), false))
-            {
-                if (propInfo.PropertyType != typeof(string))
-                    throw new InvalidOperationException("[FromClaim] attributes are only supported on string properties!");
-                //could add claim binding support for other types just like in route binding.
-
-                var attrib = propInfo.GetCustomAttribute<FromClaimAttribute>(false);
-                var claimType = attrib?.ClaimType ?? propName;
-                var forbidIfMissing = attrib?.IsRequired ?? false;
-
-                CachedFromClaimProps.Add(new(claimType, forbidIfMissing, compiledSetter));
-            }
         }
+    }
+
+    private static bool AddFromClaimPropCacheEntry(PropertyInfo propInfo, string propName, Action<object, object> compiledSetter)
+    {
+        var attrib = propInfo.GetCustomAttribute<FromClaimAttribute>(false);
+        if (attrib is not null)
+        {
+            if (propInfo.PropertyType != typeof(string))
+                throw new InvalidOperationException("[FromClaim] attributes are only supported on string properties!");
+            //could add claim binding support for other types just like in route binding.
+
+            var claimType = attrib?.ClaimType ?? propName;
+            var forbidIfMissing = attrib?.IsRequired ?? false;
+
+            CachedFromClaimProps.Add(new(claimType, forbidIfMissing, compiledSetter));
+            return true;
+        }
+        return false;
+    }
+
+    private static bool AddFromHeaderPropCacheEntry(PropertyInfo propInfo, string propName, Action<object, object> compiledSetter)
+    {
+        var attrib = propInfo.GetCustomAttribute<FromHeaderAttribute>(false);
+        if (attrib is not null)
+        {
+            if (propInfo.PropertyType != typeof(string))
+                throw new InvalidOperationException("[FromHeader] attributes are only supported on string properties!");
+            //could add header binding support for other types just like in route binding.
+
+            var headerName = attrib?.HeaderName ?? propName;
+            var forbidIfMissing = attrib?.IsRequired ?? false;
+
+            CachedFromHeaderProps.Add(new(headerName, forbidIfMissing, compiledSetter));
+            return true;
+        }
+        return false;
     }
 
     private static void AddPropCacheEntry(PropertyInfo propInfo, string propName, Action<object, object> compiledSetter)

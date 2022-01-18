9c36f2758e7649ac8655a5ae7621b73ab2ee8b66
Damith G <djnitehawk83@gmail.com>
2022-01-18 10:03:28 +0530

    add more endpoint data for exclusion filtering

--- Src/Library/Extensions/MainExtensions.cs
+++ Src/Library/Extensions/MainExtensions.cs
@@ -49,8 +49,19 @@ public static class MainExtensions
         {
             var epSettings = ep.Settings;
 
-            if (exclusionFilter?.Invoke(new(epSettings.Routes!, epSettings.Verbs!)) is true)
-                continue;
+            if (exclusionFilter?.Invoke(new(
+                ep.EndpointType,
+                epSettings.Routes!,
+                epSettings.Verbs!,
+                epSettings.AnonymousVerbs,
+                epSettings.ThrowIfValidationFails,
+                epSettings.PreBuiltUserPolicies,
+                epSettings.Roles,
+                epSettings.Permissions,
+                epSettings.AllowAnyPermission,
+                epSettings.ClaimTypes,
+                epSettings.AllowAnyClaim
+                )) is true) continue;
 
             var epName = ep.EndpointType.FullName;
 
@@ -210,11 +221,29 @@ public static class MainExtensions
 /// <summary>
 /// represents an endpoint that has been discovered during startup
 /// </summary>
-/// <param name="Routes">the route urls of the endpoint</param>
-/// <param name="Verbs">the verbs of the endpoint</param>
-public record DiscoveredEndpoint(
+/// <param name="EndpointType">the type of the discovered endpoint class</param>
+/// <param name="Routes">the routes the endpoint will match</param>
+/// <param name="Verbs">the http verbs the endpoint will be listening for</param>
+/// <param name="AnonymousVerbs">the verbs which will be allowed anonymous access to</param>
+/// <param name="ThrowIfValidationFails">whether automatic validation failure will be sent</param>
+/// <param name="Policies">the security policies for the endpoint</param>
+/// <param name="Roles">the roles which will be allowed access to</param>
+/// <param name="Permissions">the permissions which will allow access</param>
+/// <param name="AllowAnyPermission">whether any or all permissions will be required</param>
+/// <param name="Claims">the user claim types which will allow access</param>
+/// <param name="AllowAnyClaim">whether any or all claim types will be required</param>
+public record struct DiscoveredEndpoint(
+    Type EndpointType,
     IEnumerable<string> Routes,
-    IEnumerable<string> Verbs);
+    IEnumerable<string> Verbs,
+    IEnumerable<string>? AnonymousVerbs,
+    bool ThrowIfValidationFails,
+    IEnumerable<string>? Policies,
+    IEnumerable<string>? Roles,
+    IEnumerable<string>? Permissions,
+    bool AllowAnyPermission,
+    IEnumerable<string>? Claims,
+    bool AllowAnyClaim);
 
 internal class StartupTimer { }
 internal class DuplicateHandlerRegistration { }

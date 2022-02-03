b8ab73c86ea569b41a925b571e387414a88c64ee
Damith G <djnitehawk83@gmail.com>
2022-02-03 17:44:13 +0530

    change swagger security schema name

--- Src/Swagger/Extensions.cs
+++ Src/Swagger/Extensions.cs
@@ -34,7 +34,7 @@ public static class Extensions
         settings.AddSecurity("JWTBearerAuth", new OpenApiSecurityScheme
         {
             Type = OpenApiSecuritySchemeType.Http,
-            Scheme = "bearer",
+            Scheme = "Bearer",
             BearerFormat = "JWT",
             Description = "Enter a JWT token to authorize the requests..."
         });

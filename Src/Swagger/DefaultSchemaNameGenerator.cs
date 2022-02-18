848063920c242a1ca0da48896bf4d3edc2e7810d
Damith G <djnitehawk83@gmail.com>
2022-02-18 11:53:28 +0530

    generics support for swagger dtos

--- Src/Swagger/DefaultSchemaNameGenerator.cs
+++ Src/Swagger/DefaultSchemaNameGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using NJsonSchema.Generation;
+using System.Text;
 
 namespace FastEndpoints.Swagger;
 
@@ -13,9 +14,55 @@ internal class DefaultSchemaNameGenerator : ISchemaNameGenerator
 
     public string? Generate(Type type)
     {
+        bool isGeneric = type.IsGenericType;
+        string fullNameWithoutGenericArgs;
+
+        if (isGeneric)
+            fullNameWithoutGenericArgs = type.FullName![..type.FullName.IndexOf('`')];
+        else
+            fullNameWithoutGenericArgs = type.FullName!;
+
         if (shortSchemaNames)
+        {
+            var index = fullNameWithoutGenericArgs.LastIndexOf('.');
+            index = index == -1 ? 0 : index;
+            var shortName = fullNameWithoutGenericArgs[index..];
+            if (isGeneric)
+                return shortName + GenericArgString(type);
+            return shortName;
+        }
+        else
+        {
+            var sanitizedFullName = fullNameWithoutGenericArgs.Replace(".", string.Empty);
+            if (isGeneric)
+                return sanitizedFullName + GenericArgString(type);
+            return sanitizedFullName;
+        }
+
+        static string? GenericArgString(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                var sb = new StringBuilder();
+                var args = type.GetGenericArguments();
+                for (int i = 0; i < args.Length; i++)
+                {
+                    var arg = args[i];
+                    sb.Append("Of")
+                      .Append(TypeNameWithoutGenericArgs(arg))
+                      .Append(GenericArgString(arg));
+                    if (i < args.Length - 1) sb.Append("And");
+                }
+                return sb.ToString();
+            }
             return type.Name;
 
-        return type.FullName?.Replace(".", string.Empty);
+            static string TypeNameWithoutGenericArgs(Type type)
+            {
+                var index = type.Name.IndexOf('`');
+                index = index == -1 ? 0 : index;
+                return type.Name![..index];
+            }
+        }
     }
 }

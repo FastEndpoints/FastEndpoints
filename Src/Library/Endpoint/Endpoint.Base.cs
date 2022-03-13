9446627c5b69d5fcbb42aeeb3dec70dd363ab470
Damith G <djnitehawk83@gmail.com>
2022-03-13 11:20:47 +0530

    lazy init validation failure list in base endpoint class

--- Src/Library/Endpoint/Endpoint.Base.cs
+++ Src/Library/Endpoint/Endpoint.Base.cs
@@ -5,10 +5,12 @@ namespace FastEndpoints;
 
 public abstract class BaseEndpoint : IEndpoint
 {
-    protected internal HttpContext _httpContext; //this is set at the start of ExecAsync() method of each endpoint instance
-
     //key: the type of the endpoint
     internal static Dictionary<Type, string> TestURLCache { get; } = new();
+
+    protected internal HttpContext _httpContext; //this is set at the start of ExecAsync() method of each endpoint instance
+
+    private List<ValidationFailure> _failures;
 
     public EndpointDefinition Configuration { get; internal set; }
 
@@ -22,7 +24,7 @@ public abstract class BaseEndpoint : IEndpoint
     /// <summary>
     /// the list of validation failures for the current request dto
     /// </summary>
-    public List<ValidationFailure> ValidationFailures { get; } = new();
+    public List<ValidationFailure> ValidationFailures => _failures ??= new();
 
     /// <summary>
     /// use this method to configure how the endpoint should be listening to incoming requests.

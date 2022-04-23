19964ea134702e0a98cd1127447165b3585a43ca
Damith G <djnitehawk83@gmail.com>
2022-04-23 09:00:10 +0530

    release v4.1.0-beta5

--- Src/Library/Extensions/ReflectionExtensions.cs
+++ Src/Library/Extensions/ReflectionExtensions.cs
@@ -40,9 +40,10 @@ internal static class ReflectionExtensions
     internal static Type[]? GetGenericArgumentsOfType(this Type source, Type targetGeneric)
     {
         if (!targetGeneric.IsGenericType)
-            throw new ArgumentException($"{nameof(targetGeneric)} is not a valid generic type", nameof(targetGeneric));
+            throw new ArgumentException($"{nameof(targetGeneric)} is not a valid generic type!", nameof(targetGeneric));
 
         var t = source;
+
         while (t != null)
         {
             if (t.IsGenericType && t.GetGenericTypeDefinition() == targetGeneric)

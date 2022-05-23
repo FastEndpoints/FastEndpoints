abbdeec5f49a83a3455d6c7fcca780cd5f225a7e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-05-23 12:06:37 +0200

    Working on feedback from https://github.com/dj-nitehawk/FastEndpoints/issues/117#issuecomment-1133228468

---
+++ Src/Generator/TypeDescription.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace FastEndpoints.Generator;
+
+internal class TypeDescription : IEquatable<TypeDescription>
+{
+    public TypeDescription(INamedTypeSymbol symbol)
+    {
+        Namespace = symbol.ContainingNamespace.Name;
+        Name = symbol.Name;
+    }
+
+    public TypeDescription(string typeName)
+    {
+        var segments = typeName.Split('.');
+
+        Namespace = segments[0];
+        Name = segments[1];
+    }
+
+    public string Namespace { get; }
+
+    public string Name { get; }
+
+    public bool Equals(TypeDescription? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return string.Equals(Namespace, other.Namespace, StringComparison.OrdinalIgnoreCase) &&
+               string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((TypeDescription)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (StringComparer.OrdinalIgnoreCase.GetHashCode(Namespace) * 397) ^
+                   StringComparer.OrdinalIgnoreCase.GetHashCode(Name);
+        }
+    }
+
+    public static bool operator ==(TypeDescription? left, TypeDescription? right) => Equals(left, right);
+
+    public static bool operator !=(TypeDescription? left, TypeDescription? right) => !Equals(left, right);
+}

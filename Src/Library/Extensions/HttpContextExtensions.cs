933c7a698d4856064e43d7c8843724ae97f9e9d7
Damith G <djnitehawk83@gmail.com>
2022-08-31 22:01:18 +0530

    http context extensions for resolve methods

---
+++ Src/Library/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FastEndpoints;
+
+public static class HttpContextExtensions
+{
+    /// <summary>
+    /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
+    /// </summary>
+    /// <typeparam name="TService">the type of the service to resolve</typeparam>
+    public static TService? TryResolve<TService>(this HttpContext ctx) where TService : class
+        => ctx.RequestServices.GetService<TService>();
+
+    /// <summary>
+    /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
+    /// </summary>
+    /// <param name="typeOfService">the type of the service to resolve</param>
+    public static object? TryResolve(this HttpContext ctx, Type typeOfService)
+        => ctx.RequestServices.GetService(typeOfService);
+
+    /// <summary>
+    /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
+    /// </summary>
+    /// <typeparam name="TService">the type of the service to resolve</typeparam>
+    /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
+    public static TService Resolve<TService>(this HttpContext ctx) where TService : class
+        => ctx.RequestServices.GetRequiredService<TService>();
+
+    /// <summary>
+    /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
+    /// </summary>
+    /// <param name="typeOfService">the type of the service to resolve</param>
+    /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
+    public static object Resolve(this HttpContext ctx, Type typeOfService)
+        => ctx.RequestServices.GetRequiredService(typeOfService);
+}

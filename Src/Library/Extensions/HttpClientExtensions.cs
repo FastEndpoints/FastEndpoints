c6e0792816dfb9e9f1af0b6ee98f0bd7d8378a64
Anthony Stewart <anthony.l.stewart@outlook.com>
2022-09-03 21:29:26 -0500

    Add DELETEAsync HttpClient extension methods.

--- Src/Library/Extensions/HttpClientExtensions.cs
+++ Src/Library/Extensions/HttpClientExtensions.cs
@@ -202,4 +202,73 @@ public static class HttpClientExtensions
     /// <typeparam name="TResponse">the type of the response dto</typeparam>
     public static Task<(HttpResponseMessage? response, TResponse? result)> GETAsync<TEndpoint, TResponse>(this HttpClient client) where TEndpoint : BaseEndpoint
         => GETAsync<EmptyRequest, TResponse>(client, IEndpoint.TestURLFor<TEndpoint>(), new EmptyRequest());
+
+    /// <summary>
+    /// make a DELETE request using a request dto and get back a response dto.
+    /// </summary>
+    /// <typeparam name="TRequest">type of the requet dto</typeparam>
+    /// <typeparam name="TResponse">type of the response dto</typeparam>
+    /// <param name="requestUri">the route url to post to</param>
+    /// <param name="request">the request dto</param>
+    /// <exception cref="InvalidOperationException">thrown when the response body cannot be deserialized in to specified response dto type</exception>
+    public static async Task<(HttpResponseMessage? response, TResponse? result)> DELETEAsync<TRequest, TResponse>(this HttpClient client, string requestUri, TRequest request)
+    {
+        var res = await client.SendAsync(
+            new HttpRequestMessage
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri(
+                    client.BaseAddress!.ToString().TrimEnd('/') +
+                    (requestUri.StartsWith('/') ? requestUri : "/" + requestUri)),
+                Content = new StringContent(JsonSerializer.Serialize(request, SerOpts.Options), Encoding.UTF8, "application/json")
+            });
+
+        if (typeof(TResponse) == Types.EmptyResponse)
+            return (res, default(TResponse));
+
+        TResponse? body;
+
+        try
+        {
+            body = await res.Content.ReadFromJsonAsync<TResponse>(SerOpts.Options);
+        }
+        catch (JsonException)
+        {
+            var reason = $"[{res.StatusCode}] {await res.Content.ReadAsStringAsync()}";
+            throw new InvalidOperationException(
+                $"Unable to deserialize the response body as [{typeof(TResponse).FullName}]. Reason: {reason}");
+        }
+
+        return (res, body);
+    }
+
+    /// <summary>
+    /// make a DELETE request to an endpoint using auto route discovery using a request dto and get back a response dto.
+    /// </summary>
+    /// <typeparam name="TEndpoint">the type of the endpoint</typeparam>
+    /// <typeparam name="TRequest">the type of the request dto</typeparam>
+    /// <typeparam name="TResponse">the type of the response dto</typeparam>
+    /// <param name="request">the request dto</param>
+    public static Task<(HttpResponseMessage? response, TResponse? result)> DELETEAsync<TEndpoint, TRequest, TResponse>(this HttpClient client, TRequest request) where TEndpoint : BaseEndpoint
+        => DELETEAsync<TRequest, TResponse>(client, IEndpoint.TestURLFor<TEndpoint>(), request);
+
+    /// <summary>
+    /// make a DELETE request to an endpoint using auto route discovery using a request dto that does not send back a response dto.
+    /// </summary>
+    /// <typeparam name="TEndpoint">the type of the endpoint</typeparam>
+    /// <typeparam name="TRequest">the type of the request dto</typeparam>
+    /// <param name="request">the request dto</param>
+    public static async Task<HttpResponseMessage?> DELETEAsync<TEndpoint, TRequest>(this HttpClient client, TRequest request) where TEndpoint : BaseEndpoint
+    {
+        var (response, _) = await DELETEAsync<TRequest, EmptyResponse>(client, IEndpoint.TestURLFor<TEndpoint>(), request);
+        return response;
+    }
+
+    /// <summary>
+    /// make a DELETE request to an endpoint using auto route discovery without a request dto and get back a typed response dto.
+    /// </summary>
+    /// <typeparam name="TEndpoint">the type of the endpoint</typeparam>
+    /// <typeparam name="TResponse">the type of the response dto</typeparam>
+    public static Task<(HttpResponseMessage? response, TResponse? result)> DELETEAsync<TEndpoint, TResponse>(this HttpClient client) where TEndpoint : BaseEndpoint
+        => DELETEAsync<EmptyRequest, TResponse>(client, IEndpoint.TestURLFor<TEndpoint>(), new EmptyRequest());
 }

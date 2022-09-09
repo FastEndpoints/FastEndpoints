b8af7060d0232237c922c5719179e26901678997
Damith G <djnitehawk83@gmail.com>
2022-09-09 20:50:22 +0530

    swagger auth scheme name change
    closes #229

--- Src/Swagger/Extensions.cs
+++ Src/Swagger/Extensions.cs
@@ -52,7 +52,7 @@ public static class Extensions
         settings.AddAuth("JWTBearerAuth", new OpenApiSecurityScheme
         {
             Type = OpenApiSecuritySchemeType.Http,
-            Scheme = "Bearer",
+            Scheme = "bearer",
             BearerFormat = "JWT",
             Description = "Enter a JWT token to authorize the requests..."
         });

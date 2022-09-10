62ab5ce17efd1a4e43254b87be1c89f401ea2aca
Damith G <djnitehawk83@gmail.com>
2022-09-10 14:03:56 +0530

    fix validator instantiation bug in unit tests

--- Src/Library/Validator.cs
+++ Src/Library/Validator.cs
@@ -12,34 +12,34 @@ namespace FastEndpoints;
 public abstract class Validator<TRequest> : AbstractValidator<TRequest>, IServiceResolver, IEndpointValidator where TRequest : class
 {
     //todo: switch to IServiceResolver.RootServiceProvider when removing ability to register validators as scoped
-    private readonly IHttpContextAccessor accessor = IServiceResolver.RootServiceProvider.GetRequiredService<IHttpContextAccessor>();
+    private readonly IHttpContextAccessor? accessor = IServiceResolver.RootServiceProvider?.GetRequiredService<IHttpContextAccessor>();
 
     /// <summary>
     /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
     /// </summary>
     /// <typeparam name="TService">the type of the service to resolve</typeparam>
     public TService? TryResolve<TService>() where TService : class
-        => accessor.HttpContext?.RequestServices.GetService<TService>();
+        => accessor?.HttpContext?.RequestServices.GetService<TService>();
     /// <summary>
     /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
     /// </summary>
     /// <param name="typeOfService">the type of the service to resolve</param>
     public object? TryResolve(Type typeOfService)
-        => accessor.HttpContext?.RequestServices.GetService(typeOfService);
+        => accessor?.HttpContext?.RequestServices.GetService(typeOfService);
     /// <summary>
     /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
     /// </summary>
     /// <typeparam name="TService">the type of the service to resolve</typeparam>
     /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
     public TService Resolve<TService>() where TService : class
-        => accessor.HttpContext!.RequestServices.GetRequiredService<TService>();
+        => accessor!.HttpContext!.RequestServices.GetRequiredService<TService>();
     /// <summary>
     /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
     /// </summary>
     /// <param name="typeOfService">the type of the service to resolve</param>
     /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
     public object Resolve(Type typeOfService)
-        => accessor.HttpContext!.RequestServices.GetRequiredService(typeOfService);
+        => accessor!.HttpContext!.RequestServices.GetRequiredService(typeOfService);
     /// <summary>
     /// if you'd like to resolve scoped or transient services from the DI container, obtain a service scope from this method and dispose the scope when the work is complete.
     ///<para>
@@ -50,5 +50,5 @@ public abstract class Validator<TRequest> : AbstractValidator<TRequest>, IServic
     /// </para>
     /// </summary>
     public IServiceScope CreateScope()
-        => accessor.HttpContext!.RequestServices.CreateScope();
+        => accessor!.HttpContext!.RequestServices.CreateScope();
 }
--- Src/Library/changelog.md
+++ Src/Library/changelog.md
@@ -4,3 +4,4 @@
 ### FIX
 - pre/post processor collection modification bug #224
 - response dto initialization not working with array types #225
+- unable to instantiate validators for unit tests [#info](https://discord.com/channels/933662816458645504/1017889876521267263)

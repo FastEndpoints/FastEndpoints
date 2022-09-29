414cb5514f79764c19a034a80e4caf641c95d230
vpetrusevici <vladimir.petrusevici@outlook.com>
2022-09-29 19:02:52 +0300

    Use JsonValue instead of ConvertToInt64 for enum binging in query (#264)
    * Fix enum binding for other types
    
    * Use JsonValue instead of Convert.ToInt64 for enum binding
    
    * remove innecessary whitespace :)

--- Src/Library/Extensions/ReflectionExtensions.cs
+++ Src/Library/Extensions/ReflectionExtensions.cs
@@ -147,8 +147,7 @@ internal static class ReflectionExtensions
             return input => bool.TryParse(input[0], out var res) ? res : input[0];
 
         if (tProp.IsEnum)
-            return input => Enum.TryParse(tProp, input[0], true, out var res) ? Convert.ToInt64(res) : input[0];
-
+            return input => Enum.TryParse(tProp, input[0], true, out var res) ? JsonValue.Create(res) : input[0];
         if (tProp.GetInterfaces().Contains(Types.IEnumerable))
         {
             var elementType = tProp.GetGenericArguments().FirstOrDefault();

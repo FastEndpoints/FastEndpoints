f66017c6f3a8ae1646f9f5c06f6194ef2bc6df47
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2022-10-25 10:44:05 +0530

    update xml docs

--- Src/Library/Messaging/Commands/CommandHandler.cs
+++ Src/Library/Messaging/Commands/CommandHandler.cs
@@ -1,32 +1,30 @@
 ﻿namespace FastEndpoints;
 
 /// <summary>
-/// inherit this base class to handle commands sent using Request/Response pattern
-/// <para>WARNING: command handlers are singletons. DO NOT maintain state in them. Use the <c>Resolve*()</c> methods to obtain dependencies.</para>
+/// inherit this base class to handle commands [<see cref="ICommand{TResult}"/>] issued by the command bus pattern.
 /// </summary>
 /// <typeparam name="TCommand">the type of the command to handle</typeparam>
-/// <typeparam name="TResult">the type of the response result</typeparam>
+/// <typeparam name="TResult">the type of the result</typeparam>
 public abstract class FastCommandHandler<TCommand, TResult> : HandlerBase, ICommandHandler<TCommand, TResult>, IServiceResolver where TCommand : ICommand<TResult>
 {
     /// <summary>
-    /// this method will be called when an command of the specified type is executed.
+    /// this method will be called when a command of the specified type is executed.
     /// </summary>
     /// <param name="cmd">the command model/dto received</param>
     /// <param name="ct">an optional cancellation token</param>
-    public abstract Task<TResult> ExecuteAsync(TCommand cmd, CancellationToken ct);
+    public abstract Task<TResult> ExecuteAsync(TCommand cmd, CancellationToken ct = default);
 }
 
 /// <summary>
-/// inherit this base class to handle commands sent using Request/Response pattern
-/// <para>WARNING: command handlers are singletons. DO NOT maintain state in them. Use the <c>Resolve*()</c> methods to obtain dependencies.</para>
+/// inherit this base class to handle commands [<see cref="ICommand{TResult}"/>] issued by the command bus pattern.
 /// </summary>
 /// <typeparam name="TCommand">the type of the command to handle</typeparam>
 public abstract class FastCommandHandler<TCommand> : HandlerBase, ICommandHandler<TCommand>, IServiceResolver where TCommand : ICommand
 {
     /// <summary>
-    /// this method will be called when an command of the specified type is published.
+    /// this method will be called when a command of the specified type is published.
     /// </summary>
     /// <param name="cmd">the command model/dto received</param>
     /// <param name="ct">an optional cancellation token</param>
-    public abstract Task ExecuteAsync(TCommand cmd, CancellationToken ct);
+    public abstract Task ExecuteAsync(TCommand cmd, CancellationToken ct = default);
 }

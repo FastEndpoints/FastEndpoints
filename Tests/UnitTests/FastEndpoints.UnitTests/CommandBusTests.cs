0580c53988cc880158f3f18018ac4c04842d9e48
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2022-10-25 10:22:19 +0530

    unit test for command bus

---
+++ Tests/UnitTests/FastEndpoints.UnitTests/CommandBusTests.cs
@@ -0,0 +1,21 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using TestCases.CommandBusTest;
+using Web.Services;
+using Xunit;
+
+namespace FastEndpoints.UnitTests;
+
+public class CommandBusTests
+{
+    [Fact]
+    public async Task CommandExecusionWorks()
+    {
+        var command = new TestCommand { FirstName = "a", LastName = "b" };
+        var handler = new TestCommandHandler(A.Fake<ILogger<TestCommandHandler>>(), A.Fake<IEmailService>());
+
+        var res = await handler.ExecuteAsync(command, default);
+
+        res.Should().Be("a b");
+    }
+}

4dcbc2a979101ad5a3e0bf8ae5a54453111988c8
ms0815user <49184358+ms0815user@users.noreply.github.com>
2022-10-30 11:38:07 +0800

    remove readonly properties from open api (#306)
    * remove readonly properties from open api
    
    * refactor operation processor
    
    Co-authored-by: Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
    Co-authored-by: Dĵ ΝιΓΞΗΛψΚ <dj-nitehawk@users.noreply.github.com>

--- Src/Swagger/OperationProcessor.cs
+++ Src/Swagger/OperationProcessor.cs
@@ -148,7 +148,7 @@ internal class OperationProcessor : IOperationProcessor
 
         var reqDtoType = apiDescription.ParameterDescriptions.FirstOrDefault()?.Type;
         var reqDtoIsList = reqDtoType?.GetInterfaces().Contains(Types.IEnumerable);
-        var reqDtoProps = reqDtoIsList is true ? null : reqDtoType?.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+        var reqDtoProps = reqDtoIsList is true ? null : reqDtoType?.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy).ToList();
         var isGETRequest = apiDescription.HttpMethod == "GET";
 
         //store unique request param description (from each consumes/content type) for later use.
@@ -198,10 +198,10 @@ internal class OperationProcessor : IOperationProcessor
 
         if (reqDtoProps != null)
         {
-            //Remove the readonly properties from the request body
-            foreach (var p in reqDtoProps.Where(p => !p.CanWrite))
+            foreach (var p in reqDtoProps.Where(p => p.GetSetMethod()?.IsPublic is not true).ToArray()) //prop has no public setter
             {
                 RemovePropFromRequestBodyContent(p.Name, op.RequestBody?.Content, propsToRemoveFromExample);
+                reqDtoProps.Remove(p);
             }
         }
 
@@ -389,9 +389,6 @@ internal class OperationProcessor : IOperationProcessor
 
     private static bool ShouldAddQueryParam(PropertyInfo prop, List<OpenApiParameter> reqParams, bool isGETRequest)
     {
-        if (!prop.CanWrite)
-            return false;
-
         var paramName = prop.Name;
 
         foreach (var attribute in prop.GetCustomAttributes())

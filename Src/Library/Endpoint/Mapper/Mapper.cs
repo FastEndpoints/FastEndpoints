135eec9b88b7749f388eb21a29837d0ca22856d7
danjrwalsh <dan@danwalsh.ca>
2022-11-27 23:16:56 -0500

    Added missing param xml comments

--- Src/Library/Endpoint/Mapper/Mapper.cs
+++ Src/Library/Endpoint/Mapper/Mapper.cs
@@ -20,6 +20,7 @@ public abstract class Mapper<TRequest, TResponse, TEntity> : IMapper, IServiceRe
     /// override this method and place the logic for mapping the request dto to the desired domain entity
     /// </summary>
     /// <param name="r">the request dto to map from</param>
+    /// <param name="ct">a cancellation token</param>
     public virtual Task<TEntity> ToEntityAsync(TRequest r, CancellationToken ct = default) => throw new NotImplementedException($"Please override the {nameof(ToEntityAsync)} method!");
 
     /// <summary>
@@ -31,6 +32,7 @@ public abstract class Mapper<TRequest, TResponse, TEntity> : IMapper, IServiceRe
     /// override this method and place the logic for mapping a domain entity to a response dto
     /// </summary>
     /// <param name="e">the domain entity to map from</param>
+    /// <param name="ct">a cancellation token</param>
     public virtual Task<TResponse> FromEntityAsync(TEntity e, CancellationToken ct = default) => throw new NotImplementedException($"Please override the {nameof(FromEntityAsync)} method!");
 
     /// <summary>
@@ -69,6 +71,7 @@ public abstract class RequestMapper<TRequest, TEntity> : IRequestMapper, IServic
     /// override this method and place the logic for mapping the request dto to the desired domain entity
     /// </summary>
     /// <param name="r">the request dto to map from</param>
+    /// <param name="ct">a cancellation token</param>
     public virtual Task<TEntity> ToEntityAsync(TRequest r, CancellationToken ct = default) => throw new NotImplementedException($"Please override the {nameof(ToEntityAsync)} method!");
 
     ///<inheritdoc/>
@@ -100,6 +103,7 @@ public abstract class ResponseMapper<TResponse, TEntity> : IResponseMapper, ISer
     /// override this method and place the logic for mapping a domain entity to a response dto
     /// </summary>
     /// <param name="e">the domain entity to map from</param>
+    /// <param name="ct">a cancellation token</param>
     public virtual Task<TResponse> FromEntityAsync(TEntity e, CancellationToken ct = default) => throw new NotImplementedException($"Please override the {nameof(FromEntityAsync)} method!");
 
     ///<inheritdoc/>

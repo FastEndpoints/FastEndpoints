51c63a350f0951b9fca9010b14e639322efe3c8f
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-02-24 22:13:11 +0530

    benchmarks for command handlers

---
+++ Benchmark/FastEndpointsBench/CommandHandlerEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using FastEndpoints;
+
+namespace FEBench;
+
+public class Command : ICommand<EmptyResponse> { }
+
+public class CommandHandlerEndpoint1 : Endpoint<Command, EmptyResponse>
+{
+    public override void Configure()
+    {
+        Get("command-handler-1");
+        AllowAnonymous();
+    }
+
+    public async override Task HandleAsync(Command req, CancellationToken ct)
+        => await SendAsync(new EmptyResponse());
+}
+
+public class CommandHandlerEndpoint2 : Endpoint<Command, EmptyResponse>
+{
+    public override void Configure()
+    {
+        Get("command-handler-2");
+        AllowAnonymous();
+    }
+
+    public async override Task HandleAsync(Command cmd, CancellationToken ct)
+        => await SendAsync(await cmd.ExecuteAsync());
+}
+
+public class CommandHandler : CommandHandler<Command, EmptyResponse>
+{
+    public override Task<EmptyResponse> ExecuteAsync(Command command, CancellationToken ct = default)
+        => Task.FromResult(new EmptyResponse());
+}
---
+++ Benchmark/Runner/CommandHandlerBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable CA1822
+using BenchmarkDotNet.Attributes;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Runner;
+
+[MemoryDiagnoser, SimpleJob(launchCount: 1, warmupCount: 1, iterationCount: 10, invocationCount: 10000)]
+public class CommandHandlerBenchmarks
+{
+    private static HttpClient NoCommandHandlerClient { get; } = new WebApplicationFactory<FEBench.Program>().CreateClient();
+    private static HttpClient WithCommandHandlerClient { get; } = new WebApplicationFactory<FEBench.Program>().CreateClient();
+
+    [Benchmark(Baseline = true)]
+    public Task NoCommandHandler()
+    {
+        return NoCommandHandlerClient.SendAsync(
+            new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{NoCommandHandlerClient.BaseAddress}command-handler-1"),
+            });
+    }
+
+    [Benchmark]
+    public Task WithCommandHandler()
+    {
+        return WithCommandHandlerClient.SendAsync(
+            new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{WithCommandHandlerClient.BaseAddress}command-handler-2"),
+            });
+    }
+}

15581e209a8349ac41abf0f458658e09725ce851
Joakim Johansson <joakim.jhnssn@gmail.com>
2023-03-23 10:50:55 +0100

    Support execute of non-concrete command type

--- Src/Library/Messaging/Commands/CommandHandlerExecutor.cs
+++ Src/Library/Messaging/Commands/CommandHandlerExecutor.cs
@@ -1,5 +1,19 @@
 ﻿namespace FastEndpoints;
 
+internal abstract class CommandHandlerExecutorBase
+{
+    internal abstract Task Execute(ICommand command, Type handlerType, CancellationToken ct);
+}
+
+internal sealed class CommandHandlerExecutor<TCommand> : CommandHandlerExecutorBase where TCommand : ICommand
+{
+    internal override Task Execute(ICommand command, Type tCommandHandler, CancellationToken ct)
+    {
+        return ((ICommandHandler<TCommand>)Config.ServiceResolver.CreateInstance(tCommandHandler))
+            .ExecuteAsync((TCommand)command, ct);
+    }
+}
+
 internal abstract class CommandHandlerExecutorBase<TResult>
 {
     internal abstract Task<TResult> Execute(ICommand<TResult> command, Type handlerType, CancellationToken ct);

fdc02f604843bd1ac9b03a4986dce5b8cef076fc
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-04-08 16:51:01 +0530

    add `ProducesProblem()` extension method

--- Src/Library/Main/RouteHandlerBuilderExtensions.cs
+++ Src/Library/Main/RouteHandlerBuilderExtensions.cs
@@ -5,9 +5,12 @@ namespace FastEndpoints;
 
 public static class RouteHandlerBuilderExtensions
 {
+    public static RouteHandlerBuilder ProducesProblemFE<TResponse>(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
+        => hb.Produces<TResponse>(statusCode, contentType);
+
     public static RouteHandlerBuilder ProducesProblemFE(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
         => hb.ProducesProblemFE<ErrorResponse>(statusCode, contentType);
 
-    public static RouteHandlerBuilder ProducesProblemFE<TResponse>(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
-        => hb.Produces<TResponse>(statusCode, contentType);
+    public static RouteHandlerBuilder ProducesProblemDetails(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
+        => hb.ProducesProblemFE<ProblemDetails>(statusCode, contentType);
 }

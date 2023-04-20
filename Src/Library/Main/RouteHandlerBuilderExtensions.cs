e6c5a9fb55961b8ac02b57df2607e73a3df556b9
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-04-20 12:46:52 +0530

    update xml docs of extensions

--- Src/Library/Main/RouteHandlerBuilderExtensions.cs
+++ Src/Library/Main/RouteHandlerBuilderExtensions.cs
@@ -6,12 +6,29 @@ namespace FastEndpoints;
 
 public static class RouteHandlerBuilderExtensions
 {
+    /// <summary>
+    /// adds produces metadata for a given type of error response dto
+    /// </summary>
+    /// <typeparam name="TResponse">the type of the error response</typeparam>
+    /// <param name="statusCode">the status code of the error response</param>
+    /// <param name="contentType">content type header value</param>
     public static RouteHandlerBuilder ProducesProblemFE<TResponse>(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
         => hb.Produces<TResponse>(statusCode, contentType);
 
-    public static RouteHandlerBuilder ProducesProblemFE(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
-        => hb.ProducesProblemFE<ErrorResponse>(statusCode, contentType);
 
+    /// <summary>
+    /// adds produces metadata of type <see cref="ErrorResponse"/> to the endpoint description
+    /// </summary>
+    /// <param name="statusCode">the status code of the error response</param>
+    /// <param name="contentType">content type header value</param>
+    public static RouteHandlerBuilder ProducesProblemFE(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
+         => hb.ProducesProblemFE<ErrorResponse>(statusCode, contentType);
+
+    /// <summary>
+    /// adds produces metadata of type <see cref="ProblemDetails"/> (RFC7807 compatible) to the endpoint description
+    /// </summary>
+    /// <param name="statusCode">the status code of the error response</param>
+    /// <param name="contentType">content type header value</param>
     public static RouteHandlerBuilder ProducesProblemDetails(this RouteHandlerBuilder hb, int statusCode = 400, string contentType = "application/problem+json")
         => hb.ProducesProblemFE<ProblemDetails>(statusCode, contentType);
 

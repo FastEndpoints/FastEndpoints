d2f13a846f7cafb9fb010d9157d6b2ac59b50826
Kate B <kate@ekbys.com>
2023-07-22 15:53:30 +0530

    wip
    wip

---
+++ Src/Library/Messaging/Jobs/JobQueue.cs
@@ -0,0 +1,21 @@
+﻿namespace FastEndpoints;
+
+public class JobQueueBase
+{
+
+}
+
+public class JobQueue<TCommand> : JobQueueBase where TCommand : ICommand
+{
+    private static readonly ParallelOptions parallelOpts = new() { MaxDegreeOfParallelism = 1 };
+    private static readonly string jobQueueID = typeof(TCommand).FullName!;
+
+
+}
+
+public interface IJobRecord
+{
+    public string QueueID { get; set; }
+    public object Command { get; set; }
+
+}

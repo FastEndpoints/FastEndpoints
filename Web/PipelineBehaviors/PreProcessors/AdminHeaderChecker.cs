74be1ae1ae2d15d6ec3a676efbbf1622f1d9db86
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-11-23 20:27:23 +0530

    update editorconfig

--- Web/PipelineBehaviors/PreProcessors/AdminHeaderChecker.cs
+++ Web/PipelineBehaviors/PreProcessors/AdminHeaderChecker.cs
@@ -2,13 +2,13 @@ namespace Web.PipelineBehaviors.PreProcessors;
 
 public class AdminHeaderChecker : IGlobalPreProcessor
 {
-    public async Task PreProcessAsync(IPreProcessorContext context, CancellationToken ct)
+    public Task PreProcessAsync(IPreProcessorContext context, CancellationToken ct)
     {
         if (context.Request is Customers.Create.Request &&
             !context.HttpContext.Request.Headers.TryGetValue("tenant-id", out _) &&
             !context.HttpContext.Response.HasStarted)
-        {
-            await context.HttpContext.Response.SendForbiddenAsync(ct);
-        }
+            return context.HttpContext.Response.SendForbiddenAsync(ct);
+
+        return Task.CompletedTask;
     }
 }
--- Web/[Features]/TestCases/ProcessorStateTest/GlobalStatePreProcessor.cs
+++ Web/[Features]/TestCases/ProcessorStateTest/GlobalStatePreProcessor.cs
@@ -1,14 +1,12 @@
-using FluentValidation.Results;
-
 namespace TestCases.ProcessorStateTest;
 
 public class GlobalStatePreProcessor : GlobalPreProcessor<Thingy>
 {
     public GlobalStatePreProcessor(ILogger<GlobalStatePreProcessor> logger)
     {
-        logger.LogInformation("PP Injection works, and you should only see me once");
+        logger.LogDebug("PP Injection works...");
     }
-    
+
     public override Task PreProcessAsync(IPreProcessorContext context, Thingy state, CancellationToken ct)
     {
         state.GlobalStateApplied = true;

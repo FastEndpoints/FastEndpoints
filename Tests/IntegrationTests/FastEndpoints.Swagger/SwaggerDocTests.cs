9099ff18e440c0597a7874935ff6de37d9e56340
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-11-26 11:05:05 +0530

    switch to using verify for snapshot testing

--- Tests/IntegrationTests/FastEndpoints.Swagger/SwaggerDocTests.cs
+++ Tests/IntegrationTests/FastEndpoints.Swagger/SwaggerDocTests.cs
@@ -1,56 +1,53 @@
-using FluentAssertions.Json;
-
 namespace Swagger;
 
-public class SwaggerDocTests : TestClass<Fixture>
+[UsesVerify]
+public class SwaggerDocTests(Fixture f, ITestOutputHelper o) : TestClass<Fixture>(f, o)
 {
-    public SwaggerDocTests(Fixture f, ITestOutputHelper o) : base(f, o) { }
-
     [Fact]
     public async Task initial_release_doc_produces_correct_output()
     {
         var doc = await Fixture.DocGenerator.GenerateAsync("Initial Release");
+        await VerifyJson(doc.ToJson());
 
-        var json = doc.ToJson();
-        var currentDoc = JToken.Parse(json);
-
-        //await File.WriteAllTextAsync("initial-release.json", json);
-
-        var snapshot = File.ReadAllText("initial-release.json");
-        var snapshotDoc = JToken.Parse(snapshot);
-
-        currentDoc.Should().BeEquivalentTo(snapshotDoc);
+        // var currentDoc = JToken.Parse(json);
+        //
+        // //await File.WriteAllTextAsync("initial-release.json", json);
+        //
+        // var snapshot = File.ReadAllText("initial-release.json");
+        // var snapshotDoc = JToken.Parse(snapshot);
+        //
+        // currentDoc.Should().BeEquivalentTo(snapshotDoc);
     }
 
     [Fact]
     public async Task release_1_doc_produces_correct_output()
     {
         var doc = await Fixture.DocGenerator.GenerateAsync("Release 1.0");
+        await VerifyJson(doc.ToJson());
 
-        var json = doc.ToJson();
-        var currentDoc = JToken.Parse(json);
-
-        //await File.WriteAllTextAsync("release-1.json", json);
-
-        var snapshot = File.ReadAllText("release-1.json");
-        var snapshotDoc = JToken.Parse(snapshot);
-
-        currentDoc.Should().BeEquivalentTo(snapshotDoc);
+        // var currentDoc = JToken.Parse(json);
+        //
+        // //await File.WriteAllTextAsync("release-1.json", json);
+        //
+        // var snapshot = File.ReadAllText("release-1.json");
+        // var snapshotDoc = JToken.Parse(snapshot);
+        //
+        // currentDoc.Should().BeEquivalentTo(snapshotDoc);
     }
 
     [Fact]
     public async Task release_2_doc_produces_correct_output()
     {
         var doc = await Fixture.DocGenerator.GenerateAsync("Release 2.0");
+        await VerifyJson(doc.ToJson());
 
-        var json = doc.ToJson();
-        var currentDoc = JToken.Parse(json);
-
-        //await File.WriteAllTextAsync("release-2.json", json);
-
-        var snapshot = File.ReadAllText("release-2.json");
-        var snapshotDoc = JToken.Parse(snapshot);
-
-        currentDoc.Should().BeEquivalentTo(snapshotDoc);
+        // var currentDoc = JToken.Parse(json);
+        //
+        // //await File.WriteAllTextAsync("release-2.json", json);
+        //
+        // var snapshot = File.ReadAllText("release-2.json");
+        // var snapshotDoc = JToken.Parse(snapshot);
+        //
+        // currentDoc.Should().BeEquivalentTo(snapshotDoc);
     }
 }

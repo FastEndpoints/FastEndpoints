cb951dee306e0bfc44ddc749cdfd5ea4af18de9f
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2024-02-08 09:13:15 +0530

    command handler executor primary ctors

--- Src/Library/Messaging/Commands/CommandHandlerExecutor.cs
+++ Src/Library/Messaging/Commands/CommandHandlerExecutor.cs
@@ -11,17 +11,10 @@ sealed class CommandHandlerExecutor<TCommand> : CommandHandlerExecutorBase where
         => ((ICommandHandler<TCommand>)Cfg.ServiceResolver.CreateInstance(tCommandHandler)).ExecuteAsync((TCommand)command, ct);
 }
 
-sealed class FakeCommandHandlerExecutor<TCommand> : CommandHandlerExecutorBase where TCommand : ICommand
+sealed class FakeCommandHandlerExecutor<TCommand>(ICommandHandler<TCommand> handler) : CommandHandlerExecutorBase where TCommand : ICommand
 {
-    readonly ICommandHandler<TCommand> _handler;
-
-    public FakeCommandHandlerExecutor(ICommandHandler<TCommand> handler)
-    {
-        _handler = handler;
-    }
-
     internal override Task Execute(ICommand command, Type tCommandHandler, CancellationToken ct)
-        => _handler.ExecuteAsync((TCommand)command, ct);
+        => handler.ExecuteAsync((TCommand)command, ct);
 }
 
 abstract class CommandHandlerExecutorBase<TResult>
@@ -35,15 +28,9 @@ sealed class CommandHandlerExecutor<TCommand, TResult> : CommandHandlerExecutorB
         => ((ICommandHandler<TCommand, TResult>)Cfg.ServiceResolver.CreateInstance(tCommandHandler)).ExecuteAsync((TCommand)command, ct);
 }
 
-sealed class FakeCommandHandlerExecutor<TCommand, TResult> : CommandHandlerExecutorBase<TResult> where TCommand : ICommand<TResult>
+sealed class FakeCommandHandlerExecutor<TCommand, TResult>(ICommandHandler<TCommand, TResult> handler) : CommandHandlerExecutorBase<TResult>
+    where TCommand : ICommand<TResult>
 {
-    readonly ICommandHandler<TCommand, TResult> _handler;
-
-    public FakeCommandHandlerExecutor(ICommandHandler<TCommand, TResult> handler)
-    {
-        _handler = handler;
-    }
-
     internal override Task<TResult> Execute(ICommand<TResult> command, Type tCommandHandler, CancellationToken ct)
-        => _handler.ExecuteAsync((TCommand)command, ct);
+        => handler.ExecuteAsync((TCommand)command, ct);
 }

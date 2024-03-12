e5472aabb1d5293c048be0ffa2e849e1b1c025a2
Kate B <kate@ekbys.com>
2024-03-12 10:36:13 +0530

    Roll back `Microsoft.CodeAnalysis.CSharp` upgrade

--- Src/Library/Main/EndpointData.cs
+++ Src/Library/Main/EndpointData.cs
@@ -109,10 +109,10 @@ sealed class EndpointData
 
                     if (tInterfaces.Contains(Types.IHasMapper))
                     {
-                        var tMapper =
-                            t.GetGenericArgumentsOfType(Types.EndpointOf3)?[2] ??
-                            t.GetGenericArgumentsOfType(Types.EndpointWithMapperOf2)?[1] ??
-                            t.GetGenericArgumentsOfType(Types.EndpointWithOutRequestOf2)?[1];
+                        var tMapper = t.GetGenericArgumentsOfType(Types.EndpointOf3)?[2] ??
+                                      t.GetGenericArgumentsOfType(Types.EndpointWithMapperOf2)?[1] ??
+                                      t.GetGenericArgumentsOfType(Types.EndpointWithOutRequestOf2)?[1];
+
                         if (Types.IMapper.IsAssignableFrom(tMapper))
                             mapperDict[t] = tMapper;
                     }
@@ -136,6 +136,7 @@ sealed class EndpointData
                 {
                     var tEndpoint = t.GetGenericArgumentsOfType(Types.SummaryOf1)?[0] ??
                                     t.GetGenericArgumentsOfType(Types.SummaryOf2)?[0]!;
+
                     summaryDict.Add(tEndpoint, t);
 
                     continue;

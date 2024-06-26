338ca235dd8a8acaf79baa55613a5325df2c4224
Kate B <kate@ekbys.com>
2024-06-26 18:23:45 +0530

    Move `MapRemote` to `RemoteConnection` class

--- Src/Messaging/Messaging.Remote.Core/Client/RemoteConnectionCoreExtensions.cs
+++ Src/Messaging/Messaging.Remote.Core/Client/RemoteConnectionCoreExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using FastEndpoints.Messaging.Remote.Core;
 using Grpc.Core;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace FastEndpoints;
@@ -9,28 +8,8 @@ namespace FastEndpoints;
 /// <summary>
 /// client extension methods
 /// </summary>
-public static class RemoteConnectionExtensions
+public static class RemoteConnectionCoreExtensions
 {
-    /// <summary>
-    /// creates a grpc channel/connection to a remote server that hosts a known collection of command handlers and event hubs.
-    /// <para>
-    /// IMPORTANT: call the <see cref="RemoteConnectionCore.Register{TCommand, TResult}" /> method (using action <paramref name="r" />) to specify which commands are handled by
-    /// this remote server. event subscriptions can be specified using <c>app.Subscribe&lt;TEvent, TEventHandler&gt;()</c> method.
-    /// </para>
-    /// </summary>
-    /// <param name="host"></param>
-    /// <param name="remoteAddress">the address of the remote server</param>
-    /// <param name="r">a configuration action for the connection</param>
-    public static IHost MapRemote(this IHost host, string remoteAddress, Action<RemoteConnectionCore> r)
-    {
-        r(new(remoteAddress, host.Services));
-
-        var logger = host.Services.GetRequiredService<ILogger<RemoteConnectionCore>>();
-        logger.RemoteConfigured(remoteAddress, RemoteConnectionCore.RemoteMap.Count);
-
-        return host;
-    }
-
     /// <summary>
     /// creates a grpc channel/connection to a remote server that hosts a known collection of command handlers and event hubs.
     /// <para>
@@ -41,7 +20,7 @@ public static class RemoteConnectionExtensions
     /// <param name="services"></param>
     /// <param name="remoteAddress">the address of the remote server</param>
     /// <param name="r">a configuration action for the connection</param>
-    public static IServiceProvider MapRemote(this IServiceProvider services, string remoteAddress, Action<RemoteConnectionCore> r)
+    public static IServiceProvider MapRemoteCore(this IServiceProvider services, string remoteAddress, Action<RemoteConnectionCore> r)
     {
         r(new(remoteAddress, services));
 

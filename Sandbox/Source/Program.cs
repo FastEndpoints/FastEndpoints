fba142de368986a32896c0c01f4978105c49ac1f
Sjors Goering <s.goering@4dotnet.nl>
2025-01-14 16:07:04 +0100

    Cleanup Sandbox project

--- Sandbox/Source/Program.cs
+++ Sandbox/Source/Program.cs
@@ -1,24 +1,10 @@
-using Contracts;
-using Microsoft.EntityFrameworkCore;
-
 var bld = WebApplication.CreateBuilder(args);
 bld.Services
    .SwaggerDocument()
-   .AddFastEndpoints()
-   .AddJobQueues<JobRecord, JobStorageProvider>();
-
-bld.Services.AddDbContextFactory<OrderDbContext>(
-    opts =>
-    {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        var dbPath = Path.Join(path, "orders.db");
-        opts.UseSqlite($"DataSource={dbPath}");
-    });
+   .AddFastEndpoints();
 
 var app = bld.Build();
 app.UseFastEndpoints()
-   .UseJobQueues()
    .UseSwaggerGen();
 app.Run();
 

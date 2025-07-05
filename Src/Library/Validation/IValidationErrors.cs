8fda38339f626f0c9bd6756bbbd0ada32b66a17f
Michael Crawford <mbcrawfo@gmail.com>
2025-07-05 10:32:42 -0400

    Make IValidationErrors public
    Resolves #961

--- Src/Library/Validation/IValidationErrors.cs
+++ Src/Library/Validation/IValidationErrors.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 
 namespace FastEndpoints;
 
-interface IValidationErrors<T>
+public interface IValidationErrors<T>
 {
     /// <summary>
     /// validation failures collection for the endpoint

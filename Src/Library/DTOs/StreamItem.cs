2aa50e67cbd882635be25a1ba1f78c2c4fce2d01
Sjors Goering <s.goering@4dotnet.nl>
2025-07-28 17:02:26 +0200

    Made id and event in SSE optional and added extra StreamItem overload

--- Src/Library/DTOs/StreamItem.cs
+++ Src/Library/DTOs/StreamItem.cs
@@ -2,21 +2,41 @@ using System.Text.Json;
 
 namespace FastEndpoints;
 
-/// <summary>
-/// </summary>
-/// <param name="id">the id of the event</param>
-/// <param name="eventName">the name of the event</param>
-/// <param name="data">the event data</param>
-public class StreamItem(string id, string eventName, object? data)
+public class StreamItem
 {
-    /// <summary>event id</summary>
-    public string Id { get; init; } = id;
+    /// <summary>event id, omitted when NULL/summary>
+    public string? Id { get; init; }
 
-    /// <summary>event name</summary>
-    public string EventName { get; init; } = eventName;
+    /// <summary>event name, omitted when NULL</summary>
+    public string? EventName { get; init; }
 
     /// <summary>event data</summary>
-    public object? Data { get; init; } = data;
+    public object Data { get; init; }
+
+    /// <summary>reconnection time in seconds</summary>
+    public int? Retry { get; init; }
+
+    /// <param name="eventName">the name of the event</param>
+    /// <param name="data">the event data</param>
+    public StreamItem(string? eventName, object data)
+    {
+        EventName = eventName;
+        Data = data;
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="id">the id of the event</param>
+    /// <param name="eventName">the name of the event</param>
+    /// <param name="data">the event data</param>
+    /// <param name="retry">reconnection time in seconds</param>
+    public StreamItem(string? id, string? eventName, object data, int? retry = null)
+    {
+        Id = id;
+        EventName = eventName;
+        Data = data;
+        Retry = retry;
+    }
 
     /// <summary>
     /// override this method in order to take control of the serialization of the event data

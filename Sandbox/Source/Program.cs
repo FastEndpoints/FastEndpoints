2c28a3ee4e0379ecd8fd6f585230e61192b6cb09
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2025-10-01 18:06:18 +0530

    version number placement strategy

--- Sandbox/Source/Program.cs
+++ Sandbox/Source/Program.cs
@@ -7,26 +7,3 @@ var app = bld.Build();
 app.UseFastEndpoints()
    .UseSwaggerGen();
 app.Run();
-
-sealed class SimpleGater : IFeatureFlag
-{
-    public string? Name { get; set; }
-
-    public Task<bool> IsEnabledAsync(IEndpoint endpoint)
-        => Task.FromResult(Name == "HelloWorld");
-}
-
-sealed class BetaEndpoint : EndpointWithoutRequest<string>
-{
-    public override void Configure()
-    {
-        Get("/hello");
-        AllowAnonymous();
-        FeatureFlag<SimpleGater>("HelloWorld");
-    }
-
-    public override async Task HandleAsync(CancellationToken c)
-    {
-        await Send.OkAsync("hello world!");
-    }
-}

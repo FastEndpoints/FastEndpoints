29d33493dee351227fc84a77e6fe216ce3896225
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2025-12-04 10:36:40 +0530

    restore corrupted service extensions file

--- Src/Core/ServiceResolver/ServiceScopeExtensions.cs
+++ Src/Core/ServiceResolver/ServiceScopeExtensions.cs
@@ -3,18 +3,48 @@
 namespace FastEndpoints;
 
 /// <summary>
-/// keyed service extension methods for <see cref="IServiceScope" /> (.NET 8+)
+/// service scope extensions
 /// </summary>
-public static class ServiceScopeKeyedExtensions
+public static class ServiceScopeExtensions
 {
-    /// <param name="scope">the service scope</param>
+    /// <param name="scope"></param>
     extension(IServiceScope scope)
     {
         /// <summary>
         /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
         /// </summary>
         /// <typeparam name="TService">the type of the service to resolve</typeparam>
-        /// <param name="keyName">the key name for resolving keyed service</param>
+        public TService? TryResolve<TService>() where TService : class
+            => scope.ServiceProvider.GetService<TService>();
+
+        /// <summary>
+        /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
+        /// </summary>
+        /// <param name="typeOfService">the type of the service to resolve</param>
+        public object? TryResolve(Type typeOfService)
+            => scope.ServiceProvider.GetService(typeOfService);
+
+        /// <summary>
+        /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
+        /// </summary>
+        /// <typeparam name="TService">the type of the service to resolve</typeparam>
+        /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
+        public TService Resolve<TService>() where TService : class
+            => scope.ServiceProvider.GetRequiredService<TService>();
+
+        /// <summary>
+        /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
+        /// </summary>
+        /// <param name="typeOfService">the type of the service to resolve</param>
+        /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
+        public object Resolve(Type typeOfService)
+            => scope.ServiceProvider.GetRequiredService(typeOfService);
+
+        /// <summary>
+        /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
+        /// </summary>
+        /// <param name="keyName">key name</param>
+        /// <typeparam name="TService">the type of the service to resolve</typeparam>
         public TService? TryResolve<TService>(string keyName) where TService : class
             => scope.ServiceProvider.GetKeyedService<TService>(keyName);
 
@@ -22,7 +52,7 @@ public static class ServiceScopeKeyedExtensions
         /// try to resolve an instance for the given type from the dependency injection container. will return null if unresolvable.
         /// </summary>
         /// <param name="typeOfService">the type of the service to resolve</param>
-        /// <param name="keyName">the key name for resolving keyed service</param>
+        /// <param name="keyName">key name</param>
         public object? TryResolve(Type typeOfService, string keyName)
         {
             if (scope.ServiceProvider is IKeyedServiceProvider sp)
@@ -34,8 +64,8 @@ public static class ServiceScopeKeyedExtensions
         /// <summary>
         /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
         /// </summary>
+        /// <param name="keyName">key name</param>
         /// <typeparam name="TService">the type of the service to resolve</typeparam>
-        /// <param name="keyName">the key name for resolving keyed service</param>
         /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
         public TService Resolve<TService>(string keyName) where TService : class
             => scope.ServiceProvider.GetRequiredKeyedService<TService>(keyName);
@@ -44,7 +74,7 @@ public static class ServiceScopeKeyedExtensions
         /// resolve an instance for the given type from the dependency injection container. will throw if unresolvable.
         /// </summary>
         /// <param name="typeOfService">the type of the service to resolve</param>
-        /// <param name="keyName">the key name for resolving keyed service</param>
+        /// <param name="keyName">key name</param>
         /// <exception cref="InvalidOperationException">Thrown if requested service cannot be resolved</exception>
         public object Resolve(Type typeOfService, string keyName)
             => scope.ServiceProvider.GetRequiredKeyedService(typeOfService, keyName);

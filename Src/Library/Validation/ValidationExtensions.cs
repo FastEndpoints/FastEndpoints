75bb0c6ccf7b3f9c0e7d80f5aa5b0adf2b38a1fb
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2025-12-19 15:52:38 +0530

    extension block for validation

--- Src/Library/Validation/ValidationExtensions.cs
+++ Src/Library/Validation/ValidationExtensions.cs
@@ -7,87 +7,87 @@ namespace FastEndpoints;
 
 static class ValidationExtensions
 {
-    internal static bool ValidationFailed(this List<ValidationFailure> failures)
-        => failures.Count > 0;
-
-    internal static void AddError(this List<ValidationFailure> failures, ValidationFailure failure, string? reqDtoFromBodyPropName = null)
+    extension(List<ValidationFailure> failures)
     {
-        if (reqDtoFromBodyPropName?.Length > 1 && failure.PropertyName.StartsWith(reqDtoFromBodyPropName))
+        internal bool ValidationFailed()
+            => failures.Count > 0;
+
+        internal void AddError(ValidationFailure failure, string? reqDtoFromBodyPropName = null)
         {
-            failure.PropertyName = failure.PropertyName.Substring(
-                reqDtoFromBodyPropName.Length,
-                failure.PropertyName.Length - reqDtoFromBodyPropName.Length);
+            if (reqDtoFromBodyPropName?.Length > 1 && failure.PropertyName.StartsWith(reqDtoFromBodyPropName))
+            {
+                failure.PropertyName = failure.PropertyName.Substring(
+                    reqDtoFromBodyPropName.Length,
+                    failure.PropertyName.Length - reqDtoFromBodyPropName.Length);
+            }
+
+            failures.Add(failure);
         }
 
-        failures.Add(failure);
-    }
+        internal void AddError(string message, string? errorCode = null, Severity severity = Severity.Error)
+        {
+            failures.AddError(
+                new(Cfg.ErrOpts.GeneralErrorsField, message)
+                {
+                    ErrorCode = errorCode,
+                    Severity = severity
+                },
+                null);
+        }
 
-    internal static void AddError(this List<ValidationFailure> failures, string message, string? errorCode = null, Severity severity = Severity.Error)
-    {
-        failures.AddError(
-            new(Cfg.ErrOpts.GeneralErrorsField, message)
-            {
-                ErrorCode = errorCode,
-                Severity = severity
-            },
-            null);
-    }
+        internal void AddError<T>(Expression<Func<T, object?>> property,
+                                  string errorMessage,
+                                  string? errorCode = null,
+                                  Severity severity = Severity.Error,
+                                  string? reqDtoFromBodyPropName = null)
+        {
+            failures.AddError(
+                new(property.Body.GetPropertyChain(), errorMessage)
+                {
+                    ErrorCode = errorCode,
+                    Severity = severity
+                },
+                reqDtoFromBodyPropName);
+        }
 
-    internal static void AddError<T>(this List<ValidationFailure> failures,
-                                     Expression<Func<T, object?>> property,
-                                     string errorMessage,
-                                     string? errorCode = null,
-                                     Severity severity = Severity.Error,
-                                     string? reqDtoFromBodyPropName = null)
-    {
-        failures.AddError(
-            new(property.Body.GetPropertyChain(), errorMessage)
-            {
-                ErrorCode = errorCode,
-                Severity = severity
-            },
-            reqDtoFromBodyPropName);
-    }
+        internal void ThrowIfAnyErrors(int? statusCode)
+        {
+            if (failures.Count > 0)
+                throw new ValidationFailureException(failures, $"{nameof(ThrowIfAnyErrors)}() called") { StatusCode = statusCode };
+        }
 
-    internal static void ThrowIfAnyErrors(this List<ValidationFailure> failures, int? statusCode)
-    {
-        if (failures.Count > 0)
-            throw new ValidationFailureException(failures, $"{nameof(ThrowIfAnyErrors)}() called") { StatusCode = statusCode };
-    }
+        [DoesNotReturn]
+        internal void ThrowError(ValidationFailure failure, int? statusCode, string? reqDtoFromBodyPropName = null)
+        {
+            failure.PropertyName ??= Cfg.ErrOpts.GeneralErrorsField;
 
-    [DoesNotReturn]
-    internal static void ThrowError(this List<ValidationFailure> failures, ValidationFailure failure, int? statusCode, string? reqDtoFromBodyPropName = null)
-    {
-        failure.PropertyName ??= Cfg.ErrOpts.GeneralErrorsField;
+            failures.AddError(failure, reqDtoFromBodyPropName);
 
-        failures.AddError(failure, reqDtoFromBodyPropName);
+            throw new ValidationFailureException(failures, $"{nameof(ThrowError)}() called!") { StatusCode = statusCode };
+        }
 
-        throw new ValidationFailureException(failures, $"{nameof(ThrowError)}() called!") { StatusCode = statusCode };
-    }
+        [DoesNotReturn]
+        internal void ThrowError(int? statusCode,
+                                 string errorMessage,
+                                 string? errorCode = null,
+                                 Severity severity = Severity.Error)
+        {
+            failures.AddError(errorMessage, errorCode, severity);
 
-    [DoesNotReturn]
-    internal static void ThrowError(this List<ValidationFailure> failures,
+            throw new ValidationFailureException(failures, $"{nameof(ThrowError)}() called!") { StatusCode = statusCode };
+        }
+
+        [DoesNotReturn]
+        internal void ThrowError<T>(Expression<Func<T, object?>> property,
                                     int? statusCode,
                                     string errorMessage,
                                     string? errorCode = null,
-                                    Severity severity = Severity.Error)
-    {
-        failures.AddError(errorMessage, errorCode, severity);
+                                    Severity severity = Severity.Error,
+                                    string? reqDtoFromBodyPropName = null)
+        {
+            failures.AddError(property, errorMessage, errorCode, severity, reqDtoFromBodyPropName);
 
-        throw new ValidationFailureException(failures, $"{nameof(ThrowError)}() called!") { StatusCode = statusCode };
-    }
-
-    [DoesNotReturn]
-    internal static void ThrowError<T>(this List<ValidationFailure> failures,
-                                       Expression<Func<T, object?>> property,
-                                       int? statusCode,
-                                       string errorMessage,
-                                       string? errorCode = null,
-                                       Severity severity = Severity.Error,
-                                       string? reqDtoFromBodyPropName = null)
-    {
-        failures.AddError(property, errorMessage, errorCode, severity, reqDtoFromBodyPropName);
-
-        throw new ValidationFailureException(failures, $"{nameof(ThrowError)}() called") { StatusCode = statusCode };
+            throw new ValidationFailureException(failures, $"{nameof(ThrowError)}() called") { StatusCode = statusCode };
+        }
     }
 }

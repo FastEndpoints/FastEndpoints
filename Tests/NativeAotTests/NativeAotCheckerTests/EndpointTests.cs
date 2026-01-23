de270cd66ab674dd27c5fae8e2b0b1e5d431ab09
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2026-01-23 18:35:39 +0530

    update native aot slnx

--- Tests/NativeAotTests/NativeAotCheckerTests/EndpointTests.cs
+++ Tests/NativeAotTests/NativeAotCheckerTests/EndpointTests.cs
@@ -5,7 +5,7 @@ namespace NativeAotCheckerTests;
 public class EndpointTests(App app)
 {
     [Fact]
-    public async Task User_Greeting_Endpoint_Response()
+    public async Task Json_Serialization_With_Http_Post()
     {
         var (rsp, res) = await app.Client.POSTAsync<GreetingEndpoint, GreetingRequest, GreetingResponse>(
                              new()

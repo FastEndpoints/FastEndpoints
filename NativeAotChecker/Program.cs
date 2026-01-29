961bbfd4a30d943b5cbd5f77260688c7f1977597
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2026-01-29 09:18:54 +0530

    add job queue endpoint to aotchcker

--- NativeAotChecker/Program.cs
+++ NativeAotChecker/Program.cs
@@ -1,13 +1,17 @@
 using System.Text.Json.Serialization;
 using NativeAotChecker;
+using NativeAotChecker.Endpoints;
 
 var bld = WebApplication.CreateSlimBuilder(args);
-bld.Services.AddFastEndpoints(o => o.SourceGeneratorDiscoveredTypes = DiscoveredTypes.All);
+bld.Services
+   .AddFastEndpoints(o => o.SourceGeneratorDiscoveredTypes = DiscoveredTypes.All)
+   .AddJobQueues<Job, JobStorage>();
 bld.Services.ConfigureHttpJsonOptions(o => o.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default));
 
 var app = bld.Build();
 app.MapGet("healthy", () => Results.Ok());
 app.UseFastEndpoints(c => c.Binding.ReflectionCache.AddFromNativeAotChecker());
+app.UseJobQueues(o => o.StorageProbeDelay = TimeSpan.FromMilliseconds(50));
 app.Run();
 
 //needed by the hidden /_test_url_cache_ endpoint

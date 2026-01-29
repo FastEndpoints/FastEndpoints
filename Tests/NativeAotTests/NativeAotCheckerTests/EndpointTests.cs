7fdbd9b7db17eff95bd26a13778ca66db8f97080
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2026-01-29 18:19:02 +0530

    add job queue test to aot checker

--- Tests/NativeAotTests/NativeAotCheckerTests/EndpointTests.cs
+++ Tests/NativeAotTests/NativeAotCheckerTests/EndpointTests.cs
@@ -65,4 +65,30 @@ public class EndpointTests(App app)
         else
             Assert.Fail(err);
     }
+
+    [Fact]
+    public async Task Queue_Jobs_That_Return_Results()
+    {
+        var ids = new List<(Guid id, Task<TestResult<string>> responseTask)>();
+
+        for (var i = 0; i < 100; i++)
+        {
+            var id = Guid.NewGuid();
+            var task = app.Client.GETAsync<JobQueueEndpoint, JobQueueRequest, string>(new() { Id = id });
+            ids.Add((id, task));
+        }
+
+        var results = await Task.WhenAll(ids.Select(i => i.responseTask));
+
+        for (var i = 0; i < 100; i++)
+        {
+            var (id, _) = ids[i];
+            var (rsp, res, err) = results[i];
+
+            if (!rsp.IsSuccessStatusCode)
+                Assert.Fail(err);
+
+            res.ShouldBe(id.ToString());
+        }
+    }
 }

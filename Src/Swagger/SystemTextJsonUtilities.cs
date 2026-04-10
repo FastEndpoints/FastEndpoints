2ad9f0009974658d505ece56a5ae8b8fa0485f0b
Dhyan <124780292+dhypa@users.noreply.github.com>
2026-04-10 10:21:02 +0100

    Fix package versioning and remove weird reflection (#1086)
    * Add conditional dependency selection for netstandard2.1 in FastEndpoints.Core
    
    * Remove all the reflection possible from SystemTextJsonUtilities.cs and use STJ types
    
    * clean up swagger stj utility
    
    * drop `netstandard2.1` target
    
    * clean up package props
    
    ---------
    
    Co-authored-by: Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>

--- Src/Swagger/SystemTextJsonUtilities.cs
+++ Src/Swagger/SystemTextJsonUtilities.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="SystemTextJsonUtilities.cs" company="NJsonSchema">
 //     Copyright (c) Rico Suter. All rights reserved.
 // </copyright>
@@ -6,11 +6,9 @@
 // <author>Rico Suter, mail@rsuter.com</author>
 //-----------------------------------------------------------------------
 
-using Namotion.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
-using System.Collections;
 using System.Reflection;
 
 namespace NJsonSchema.Generation;
@@ -18,25 +16,17 @@ namespace NJsonSchema.Generation;
 /// <summary>
 /// Utility methods for dealing with System.Text.Json.
 /// </summary>
-public static class SystemTextJsonUtilities
+static class SystemTextJsonUtilities
 {
     /// <summary>
     /// Converts System.Text.Json serializer options to Newtonsoft JSON settings.
     /// </summary>
     /// <param name="serializerOptions">The options.</param>
     /// <returns>The settings.</returns>
-    public static JsonSerializerSettings ConvertJsonOptionsToNewtonsoftSettings(dynamic serializerOptions)
+    public static JsonSerializerSettings ConvertJsonOptionsToNewtonsoftSettings(System.Text.Json.JsonSerializerOptions serializerOptions)
     {
-        var settings = new JsonSerializerSettings
-        {
-            ContractResolver = new SystemTextJsonContractResolver(serializerOptions)
-        };
-
-        var jsonStringEnumConverter = ((IEnumerable)serializerOptions.Converters)
-                                      .OfType<object>()
-                                      .FirstOrDefault(
-                                          c => c.GetType()
-                                                .IsAssignableToTypeName("System.Text.Json.Serialization.JsonStringEnumConverter", TypeNameStyle.FullName));
+        var settings = new JsonSerializerSettings { ContractResolver = new SystemTextJsonContractResolver(serializerOptions) };
+        var jsonStringEnumConverter = serializerOptions.Converters.OfType<System.Text.Json.Serialization.JsonStringEnumConverter>().FirstOrDefault();
 
         if (jsonStringEnumConverter == null)
             return settings;
@@ -47,16 +37,16 @@ public static class SystemTextJsonUtilities
         return settings;
     }
 
-    static bool IsCamelCaseEnumNamingPolicy(object jsonStringEnumConverter)
+    static bool IsCamelCaseEnumNamingPolicy(System.Text.Json.Serialization.JsonStringEnumConverter jsonStringEnumConverter)
     {
         try
         {
-            var enumNamingPolicy = jsonStringEnumConverter
-                                   .GetType().GetRuntimeFields()
-                                   .FirstOrDefault(x => x.FieldType.FullName == "System.Text.Json.JsonNamingPolicy")?
-                                   .GetValue(jsonStringEnumConverter);
+            var enumNamingPolicy = jsonStringEnumConverter.GetType()
+                                                          .GetRuntimeFields()
+                                                          .FirstOrDefault(x => x.FieldType == typeof(System.Text.Json.JsonNamingPolicy))?
+                                                          .GetValue(jsonStringEnumConverter) as System.Text.Json.JsonNamingPolicy;
 
-            return enumNamingPolicy is not null && enumNamingPolicy.GetType().FullName == "System.Text.Json.JsonCamelCaseNamingPolicy";
+            return enumNamingPolicy == System.Text.Json.JsonNamingPolicy.CamelCase;
         }
         catch
         {
@@ -64,36 +54,22 @@ public static class SystemTextJsonUtilities
         }
     }
 
-    sealed class SystemTextJsonContractResolver(dynamic serializerOptions) : DefaultContractResolver
+    sealed class SystemTextJsonContractResolver(System.Text.Json.JsonSerializerOptions serializerOptions) : DefaultContractResolver
     {
         protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
         {
-            var attributes = member.GetCustomAttributes(true);
-
             var property = base.CreateProperty(member, memberSerialization);
 
-            var propertyIgnored = false;
-            var jsonIgnoreAttribute = attributes.FirstAssignableToTypeNameOrDefault("System.Text.Json.Serialization.JsonIgnoreAttribute");
-
-            if (jsonIgnoreAttribute != null)
-            {
-                var condition = jsonIgnoreAttribute.TryGetPropertyValue<object>("Condition");
-                if (condition is null || condition.ToString() == "Always")
-                    propertyIgnored = true;
-            }
-
-            var hasToHeaderAttribute = attributes.FirstAssignableToTypeNameOrDefault("FastEndpoints.ToHeaderAttribute") is not null;
-            var hasJsonExtensionDataAttribute = attributes.FirstAssignableToTypeNameOrDefault("System.Text.Json.Serialization.JsonExtensionDataAttribute") is not null;
-
-            property.Ignored = propertyIgnored || hasJsonExtensionDataAttribute || hasToHeaderAttribute;
+            property.Ignored |=
+                member.GetCustomAttribute<System.Text.Json.Serialization.JsonIgnoreAttribute>(true)?.Condition == System.Text.Json.Serialization.JsonIgnoreCondition.Always ||
+                member.IsDefined(typeof(System.Text.Json.Serialization.JsonExtensionDataAttribute), true) ||
+                member.IsDefined(typeof(FastEndpoints.ToHeaderAttribute), true);
 
             if (serializerOptions.PropertyNamingPolicy != null)
                 property.PropertyName = serializerOptions.PropertyNamingPolicy.ConvertName(member.Name);
 
-            dynamic? jsonPropertyNameAttribute = attributes.FirstAssignableToTypeNameOrDefault("System.Text.Json.Serialization.JsonPropertyNameAttribute");
-
-            if (jsonPropertyNameAttribute is not null && !string.IsNullOrEmpty(jsonPropertyNameAttribute.Name))
-                property.PropertyName = jsonPropertyNameAttribute?.Name;
+            if (member.GetCustomAttribute<System.Text.Json.Serialization.JsonPropertyNameAttribute>(true) is { Name: { Length: > 0 } name })
+                property.PropertyName = name;
 
             return property;
         }

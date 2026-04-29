228ee3e6f8e3908ac6e42cf97ea65c4307ac5be8
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2026-04-29 18:05:19 +0530

    cache constructor defaults

--- Src/OpenApi/ConstructorDefaultExtensions.cs
+++ Src/OpenApi/ConstructorDefaultExtensions.cs
@@ -1,9 +1,12 @@
+using System.Collections.Concurrent;
 using System.Reflection;
 
 namespace FastEndpoints.OpenApi;
 
 static class ConstructorDefaultExtensions
 {
+    static readonly ConcurrentDictionary<Type, IReadOnlyDictionary<string, object?>> _constructorDefaultCache = new();
+
     internal static object? GetParentCtorDefaultValue(this PropertyInfo property)
     {
         var parentType = property.DeclaringType;
@@ -11,11 +14,28 @@ static class ConstructorDefaultExtensions
         if (parentType?.IsClass is not true)
             return null;
 
-        return parentType.GetConstructors()
-                         .Select(c => c.GetParameters())
-                         .MaxBy(p => p.Length)?
-                         .SingleOrDefault(
-                             p => p.HasDefaultValue &&
-                                  p.Name?.Equals(property.Name, StringComparison.OrdinalIgnoreCase) is true)?.DefaultValue;
+        var constructorDefaults = _constructorDefaultCache.GetOrAdd(parentType, CreateConstructorDefaultMap);
+
+        return constructorDefaults.GetValueOrDefault(property.Name);
+    }
+
+    static IReadOnlyDictionary<string, object?> CreateConstructorDefaultMap(Type type)
+    {
+        var parameters = type.GetConstructors()
+                             .Select(c => c.GetParameters())
+                             .MaxBy(p => p.Length);
+
+        if (parameters is null)
+            return new Dictionary<string, object?>(StringComparer.OrdinalIgnoreCase);
+
+        var defaults = new Dictionary<string, object?>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var parameter in parameters)
+        {
+            if (parameter.HasDefaultValue && parameter.Name is not null)
+                defaults[parameter.Name] = parameter.DefaultValue;
+        }
+
+        return defaults;
     }
 }
